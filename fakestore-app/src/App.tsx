87c414c1549e040d11205b79db4c0220870ef503
Olep8h <panov@student.vspj.cz>
2023-07-24 17:18:33 +0200

    create main logic in App

--- fakestore-app/src/App.tsx
+++ fakestore-app/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Product from './components/Product';
+import Cart from './components/Cart';
 
-function App() {
-  return (
-    <div className="App">
-         <header className="App-header">Fake Store App</header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [products, setProducts] = useState<{ id: number; title: string; price: number }[]>([]);
+    const [cartItems, setCartItems] = useState<{ id: number; title: string; price: number }[]>([]);
+
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+            .then((response) => setProducts(response.data))
+            .catch((error) => console.error('Error fetching products:', error));
+    }, []);
+
+    const addToCart = (product: { id: number; title: string; price: number }) => {
+        setCartItems((prevCartItems) => [...prevCartItems, product]);
+    };
+
+    return (
+        <div>
+            <div>
+                <h2>Products</h2>
+                {products.map((product) => (
+                    <Product key={product.id} product={product} addToCart={addToCart} />
+                ))}
+            </div>
+            <div>
+                <Cart cartItems={cartItems} />
+            </div>
+        </div>
+    );
+};
 
 export default App;
