5c9f382a0f494919b9e54bc9ee728971d86d0205
Olep8h <panov@student.vspj.cz>
2023-07-25 10:14:39 +0200

    work with modal window and product image display and styling

--- fakestore-app/src/App.tsx
+++ fakestore-app/src/App.tsx
@@ -2,42 +2,51 @@ import React, { useEffect, useState } from 'react';
 import { fetchProducts } from './services/fakestoreApi';
 import Product from './components/Product';
 import Cart from './components/Cart';
-import { validateProductResponse } from './utils/validation';
-import { useCart } from './hooks/useCart';
 import SuccessMessage from './components/SuccessMessage';
 import { Link, Routes, Route } from 'react-router-dom';
-
-interface Product {
-    id: number;
-    title: string;
-    price: number;
-}
 
 const App: React.FC = () => {
     const [products, setProducts] = useState<Product[]>([]);
     const [error, setError] = useState<string>('');
     const [successMessage, setSuccessMessage] = useState<string>('');
+    const [cartItems, setCartItems] = useState<Product[]>([]); // Cart items stored in the App component state
+    const [timerId, setTimerId] = useState<number | null>(null);
 
     useEffect(() => {
         fetchProducts()
-            .then(validateProductResponse)
             .then((data) => setProducts(data))
             .catch((error) => setError(error.message));
     }, []);
 
-    const { cartItems, addToCart } = useCart([]);
 
-    const showSuccessMessage = (productName: string) => {
-        setSuccessMessage(`Successfully added ${productName} to the cart.`);
-        setTimeout(() => {
+
+    const showSuccessMessage = (mess: string) => {
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+        setSuccessMessage(mess);
+        const newTimerId = setTimeout(() => {
             setSuccessMessage('');
-        }, 3000); // Display the message for 3 seconds and then hide it
+        }, 5000);
+
+        setTimerId(newTimerId as any);
+    };
+
+
+
+    const handleAddToCart = (product: Product) => {
+        if (!cartItems.some((item) => item.id === product.id)) {
+            setCartItems([...cartItems, product]); // Update the cartItems state manually
+            showSuccessMessage(`Successfully added ${product.title} to the cart.`);
+        } else {
+            showSuccessMessage(`${product.title} is already in the cart.`);
+        }
     };
 
     return (
-        <div>
-            <nav>
-                <ul>
+        <div className="min-h-screen bg-gray-100">
+            <nav className="p-4 bg-white shadow">
+                <ul className="flex justify-end space-x-4">
                     <li>
                         <Link to="/">Products</Link>
                     </li>
@@ -48,25 +57,30 @@ const App: React.FC = () => {
             </nav>
 
             {error ? (
-                <div>Error: {error}</div>
+                <div className="p-4 text-red-600">{error}</div>
             ) : (
                 <>
                     <Routes>
                         <Route
                             path="/"
                             element={
-                                <div>
-                                    <h2>Products</h2>
-                                    {products.map((product) => (
-                                        <Product
-                                            key={product.id}
-                                            product={product}
-                                            onAddToCart={() => {
-                                                addToCart(product);
-                                                showSuccessMessage(product.title);
-                                            }}
-                                        />
-                                    ))}
+                                <div className="p-4">
+                                    <h2 className="text-2xl font-semibold">Products</h2>
+                                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                                        {products.map((product) => (
+                                            <Product
+                                                key={product.id}
+                                                product={{
+                                                    id: product.id,
+                                                    title: product.title,
+                                                    price: product.price,
+                                                    image: product.image
+                                                }}
+                                                onAddToCart={() => handleAddToCart(product)}
+                                            />
+
+                                        ))}
+                                    </div>
                                 </div>
                             }
                         />
