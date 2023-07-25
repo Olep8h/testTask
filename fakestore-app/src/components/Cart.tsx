902e5cd9edc42110067ebfab4ed962a6e524a3b7
Olep8h <panov@student.vspj.cz>
2023-07-25 10:08:36 +0200

    some styling + image

--- fakestore-app/src/components/Cart.tsx
+++ fakestore-app/src/components/Cart.tsx
@@ -2,31 +2,30 @@ import React from 'react';
 import CartItem from './CartItem';
 
 interface CartProps {
-    cartItems: { id: number; title: string; price: number }[];
+    cartItems: { id: number; title: string; price: number, image: string }[];
 }
 
 const Cart: React.FC<CartProps> = ({ cartItems }) => {
     const totalValue = cartItems.reduce((total, item) => total + item.price, 0);
 
     return (
-        <div>
-            <h2>Cart</h2>
-            <table>
+        <div className="p-4">
+            <h2 className="text-2xl font-semibold">Cart</h2>
+            <table className="w-full mt-4">
                 <thead>
                 <tr>
-                    <th>Product</th>
-                    <th>Price</th>
+                    <th className="px-4 py-2 font-medium text-left border-b">Product</th>
+                    <th className="px-4 py-2 font-medium text-left border-b">Price</th>
                 </tr>
                 </thead>
                 <tbody>
                 {cartItems.map((item) => (
-                    <CartItem key={item.id} item={item} />
+                    <CartItem key={item.id} item={item}/>
                 ))}
                 </tbody>
             </table>
-            <p>Total: ${totalValue}</p>
+            <p className="mt-4 text-lg font-semibold">Total: ${totalValue.toFixed(2)}</p>
         </div>
     );
 };
-
 export default Cart;
--- fakestore-app/src/components/CartItem.tsx
+++ fakestore-app/src/components/CartItem.tsx
@@ -5,14 +5,21 @@ interface CartItemProps {
         id: number;
         title: string;
         price: number;
+        image: string;
+
     };
 }
 
 const CartItem: React.FC<CartItemProps> = ({ item }) => {
     return (
-        <tr>
-            <td>{item.title}</td>
-            <td>${item.price}</td>
+        <tr className="border-b">
+            <td className="px-4 py-2">
+                <div className="flex items-center">
+                    <img src={item.image} alt={item.title} className="w-14 h-14 mr-2" /> {/* Display the image */}
+                    {item.title}
+                </div>
+            </td>
+            <td className="px-4 py-2">${item.price}</td>
         </tr>
     );
 };
