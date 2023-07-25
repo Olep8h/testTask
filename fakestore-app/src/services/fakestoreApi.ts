6eb3e51eed7761fda547875771766a59b89b5b6d
Olep8h <panov@student.vspj.cz>
2023-07-25 10:11:52 +0200

    + productsWithImage

--- fakestore-app/src/services/fakestoreApi.ts
+++ fakestore-app/src/services/fakestoreApi.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
-export const fetchProducts = async () => {
+
+export async function fetchProducts(): Promise<Product[]> {
     try {
-        const response = await axios.get('https://fakestoreapi.com/products');
-        return response.data;
+        const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
+        const data = response.data;
+
+        const productsWithImage = data.map((product) => ({
+            ...product,
+            image: product.image // Assuming the API already provides the image property
+        }));
+
+        return productsWithImage;
     } catch (error) {
-        throw new Error('Error fetching products');
+        // Handle any error that occurred during the API call
+        console.error('Error fetching products:', error);
+        throw error; // Rethrow the error to be handled by the calling code
     }
-};
+}
